350d8dd0cecef5d23f5fd380f36e141b87376650
Conner <hetti031@morris.umn.edu>
2022-03-23 20:12:05 -0500

    Implement wall jumping
    The player now jumps left or right depending on which side they approach the wall from. They also no longer have the right/left jump when standing on top of the wall.
    
    Todo: clean up conditional logic for wall jumping and fix low gravity when jumping while pressed against the wall.
    
    Co-authored-by: burge337 <burge337@morris.umn.edu>

--- Assets/GorillaController.cs
+++ Assets/GorillaController.cs
@@ -9,8 +9,9 @@ public class GorillaController : MonoBehaviour
     [SerializeField] private Collider2D gorillaCollider;
     [SerializeField] private SpriteRenderer mainSpriteRenderer;
     [SerializeField] private int moveSpeed;
+    private GameObject collisionObject;
     private int jumpCount = 1;
-
+    float playerCollisionDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,13 +35,46 @@ public class GorillaController : MonoBehaviour
             gorillaRigidbody.AddForce(new Vector2(0, 1500));
             jumpCount = 0;
         }
+        if (collisionObject != null && gorillaCollider.IsTouching(collisionObject.GetComponent<Collider2D>()) && collisionObject.CompareTag("wall") && Input.GetKeyDown("w"))
+        {
+            // bounds.size.y seems to take into account transform position. Changing to just this is getting the proper height of the wall.
+            
+            if (gorillaTransform.position.x < collisionObject.transform.position.x 
+                && gorillaTransform.position.y < collisionObject.GetComponent<Collider2D>().bounds.size.y)
+            {
+                gorillaRigidbody.AddForce(new Vector2(-1500, 0));
+            } else if (gorillaRigidbody.position.x > collisionObject.transform.position.x
+                && gorillaTransform.position.y < collisionObject.GetComponent<Collider2D>().bounds.size.y)
+            {
+                gorillaRigidbody.AddForce(new Vector2(1500, 0));
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        collisionObject = collision.gameObject;
+        playerCollisionDirection = gorillaRigidbody.velocity.x;
+        Debug.Log(playerCollisionDirection);
         if (collision.gameObject.CompareTag("jumpReset"))
         {
             jumpCount = 1;
         }
+
+        if (collision.gameObject.CompareTag("wall"))
+        {
+            jumpCount = 1;
+            gorillaRigidbody.gravityScale = 2.5f;
+        }
+        if (Input.GetKeyDown("w") && jumpCount == 1)
+        {
+            gorillaRigidbody.AddForce(new Vector2(-1500, 0));
+            jumpCount = 0;
+        }
+
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        gorillaRigidbody.gravityScale = 10;
     }
+}
