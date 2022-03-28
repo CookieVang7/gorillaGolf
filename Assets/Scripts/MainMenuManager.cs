c182cfc7fd95a64c9011282c35db65dbbe9eaef2
Cookie Vang <vang2660@morris.umn.edu>
2022-03-28 17:39:18 -0500

    Add main menu script

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private Button startGame;
+    [SerializeField] private Button credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //startGame.onClick.AddListener(
+        //    () => {
+        //        LoadingScreen.LoadScene("JoshScene");
+        //    });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
