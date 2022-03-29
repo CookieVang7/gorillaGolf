744127ea3554395a5ec1b8ac7e5617a340711c95
Cookie Vang <vang2660@morris.umn.edu>
2022-03-28 19:21:11 -0500

    Add loading scene and main menu
    Co-authored-by: Jeklund99 <eklun124@umn.edu>

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -6,15 +6,28 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private Button startGame;
-    [SerializeField] private Button credits;
+    [SerializeField] private Button openCredits;
+    [SerializeField] private Button closeCredits;
+    [SerializeField] private GameObject creditsUI;
 
     // Start is called before the first frame update
     void Start()
     {
-        //startGame.onClick.AddListener(
-        //    () => {
-        //        LoadingScreen.LoadScene("JoshScene");
-        //    });
+        startGame.onClick.AddListener(
+            () =>
+            {
+                LoadingScreen.LoadScene("JoshLevel");
+            });
+
+        openCredits.onClick.AddListener(
+            () => {
+                creditsUI.SetActive(true);
+            });
+
+        closeCredits.onClick.AddListener(
+            () => {
+                creditsUI.SetActive(false);
+            });
     }
 
     // Update is called once per frame
