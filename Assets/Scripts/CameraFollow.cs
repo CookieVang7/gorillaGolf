8a7f0ad3a9aedd9af79ae1884e0a92de138d9c93
Conner <hetti031@morris.umn.edu>
2022-03-29 10:50:20 -0500

    Create new scene with new level + fix camera script
    Tried working with a tilemap for the majority of level creation. Also changed the camera follow script to follow on the y - axis and the x - axis

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -20,6 +20,7 @@ public class CameraFollow : MonoBehaviour
         Vector3 currentPosition = mainTransform.position;
         Vector3 smoothPosition = Vector3.SmoothDamp(mainTransform.position, target.position, ref velocity, 0.15f);
         currentPosition.x = smoothPosition.x;
+        currentPosition.y = smoothPosition.y;
         mainTransform.position = currentPosition;
     }
 }
