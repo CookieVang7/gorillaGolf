32a35ae7ff0704a24cdfb5b442ff4961ff532207
Conner <hetti031@morris.umn.edu>
2022-04-08 23:00:10 -0500

    Another Potential Level design + Camera Move script
    Testing to see how a level could be if at some point the camera moved to reveal more of the level. This is just a test and I still hopefully plan on Josh making the third Level for the sand course, but I want to test some Level ideas to see which ones are more interesting / more appropriate for the difficulty we are looking for in the sand course

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] Transform CameraTransform;
+    [SerializeField] float cameraX;
+    [SerializeField] float cameraY;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("Gorilla")){
+            Debug.Log("This is working");
+            //collision.gameObject.GetComponent<Transform>().position = new Vector3(cameraX, cameraY, 0);
+            CameraTransform.position = new Vector3(cameraX, cameraY, -50.7f);
+        }
+    }
+
+}
