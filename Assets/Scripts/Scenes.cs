2893fa4eeefe1a86ddfdc9d3b17f061af2500fd3
Ben Burgess <burge337@morris.umn.edu>
2022-04-11 18:47:56 -0500

    Setup level transition tags and establish level naming consistency
    Now levels are numbered according to the their themes in increasing difficulty.
    
    Also changed the ball prefab to have unfrozen the z-axis rotation so the ball actually rolls.

--- Assets/Scripts/Scenes.cs
+++ Assets/Scripts/Scenes.cs
@@ -1,12 +1,14 @@
 
-
     public enum Scenes
     {
-        JoshLevel,
+        JungleLevel3,
         TutorialLevel,
-        ConnerLevel2,
+        JungleLevel2,
         SandLevel2,
-        SandLevel3
+        SandLevel3,
+        SnowLevel1,
+        SnowLevel2,
+        SnowLevel3
 
 }
 
@@ -16,16 +18,23 @@
         {
             switch (me)
             {
-                case Scenes.JoshLevel:
-                    return "JoshLevel";
+                case Scenes.JungleLevel3:
+                    return "JungleLevel3";
                 case Scenes.TutorialLevel:
                     return "TutorialLevel";
-                case Scenes.ConnerLevel2:
-                    return "ConnerLevel2";
+                case Scenes.JungleLevel2:
+                    return "JungleLevel2";
                 case Scenes.SandLevel2:
                      return "SandLevel2";
                 case Scenes.SandLevel3:
                    return "SandLevel3";
+                case Scenes.SnowLevel1:
+                    return "SnowLevel1";
+                case Scenes.SnowLevel2:
+                    return "SnowLevel2";
+                case Scenes.SnowLevel3:
+                    return "SnowLevel3";
+
             default:
                     return "Scene Not Found";
             }
