52a2098add5aceafa87746995a5a49226c3c2cdf
Joshua Eklund <eklun124@umn.edu>
2022-04-22 12:52:03 -0500

    Work on implementing par/scoring
    This commit works on implement a par/scoring system for each hole. Just need to finish the layout for the final winscreen

--- Assets/Scripts/FinalWinScreen.cs
+++ Assets/Scripts/FinalWinScreen.cs
@@ -10,11 +10,13 @@ public class FinalWinScreen : MonoBehaviour
     [SerializeField] private Button nextLevel;
     [SerializeField] private Button replay;
     [SerializeField] private Scenes nextScene;
-    
+    [SerializeField] private TMP_Text par;
     [SerializeField] private TMP_Text strokes;
     [SerializeField] private TMP_Text deaths;
     [SerializeField] private TMP_Text completedStrokes;
     [SerializeField] private TMP_Text incompleteStokes;
+    [SerializeField] private TMP_Text score;
+    [SerializeField] private GameUI gameUI;
     void Start()
     {
         DeathCounter.totalCompletedStrokes = DeathCounter.totalCompletedStrokes + DragNShoot.hitCount;
@@ -22,6 +24,14 @@ public class FinalWinScreen : MonoBehaviour
         replay.onClick.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
         strokes.text = "Strokes: " + DragNShoot.hitCount;
         deaths.text = "Deaths: " + DeathCounter.deathCount;
+        par.text = "Par: " + gameUI.getPar();
+        int score = DragNShoot.hitCount - gameUI.getPar();
+        if(score > 0) {
+             this.score.text = "Score: +" + score;
+        }
+        else {
+            this.score.text = "Score: " + score;
+        }
         completedStrokes.text = "Total Completed Strokes: " + DeathCounter.totalCompletedStrokes;
         incompleteStokes.text = "Total Strokes: " + DeathCounter.totalDeathStrokes;
     }
