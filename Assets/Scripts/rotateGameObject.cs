eaca39e9863acf6797bf573ba1f74b2d06e6c4c8
Ben Burgess <burge337@morris.umn.edu>
2022-04-23 17:43:39 -0500

    Add potential space background

---
+++ Assets/Scripts/rotateGameObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// rotates the game object the script is attached to back and forth along the z-axis between the input angles
+
+public class rotateGameObject : MonoBehaviour
+{
+    [SerializeField] private bool rotatingTowardStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, Mathf.PingPong(Time.time * 20, 90) - 90);
+        Debug.Log(transform.rotation.z);
+    }
+}
