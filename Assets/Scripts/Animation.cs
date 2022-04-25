a8bf457647126625bd6af94193f6b38ab7c89615
Cookie Vang <vang2660@morris.umn.edu>
2022-04-24 20:43:58 -0500

    Implement wall jumping animation
    Co-authored-by: kahnurd <hetti031@morris.umn.edu>

--- Assets/Scripts/Animation.cs
+++ Assets/Scripts/Animation.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Animation : MonoBehaviour
 {
+
+    [SerializeField] private Animator animator;
+    [SerializeField] Sprite smileIdle;
+    [SerializeField] Sprite idle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,22 @@ public class Animation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void updateAnimation(SpriteRenderer gorillaSprite, Animator animator) 
+    {
+        if (DragNShoot.closeToBall)
+        {
+            Debug.Log("Please god work");
+            //gorillaSprite.sprite = smileIdle;
+            animator.SetLayerWeight(animator.GetLayerIndex("Smile Layer"), 1);
+            animator.SetLayerWeight(animator.GetLayerIndex("Base Layer"), 0);
+        }
+        else
+        {
+            //gorillaSprite.sprite = idle;
+            animator.SetLayerWeight(animator.GetLayerIndex("Smile Layer"), 0);
+            animator.SetLayerWeight(animator.GetLayerIndex("Base Layer"), 1);
+        }
     }
 }
